4d52d9587db54159ff6141568f1e1f1ffa00011f
Sulav Pokhrel <103086640+TrainYourFingers@users.noreply.github.com>
2023-08-08 20:03:40 +0545

    final Commit

--- second-episode/better_guessing_game/src/main.rs
+++ second-episode/better_guessing_game/src/main.rs
@@ -26,4 +26,3 @@ fn main() {
         Ordering::Equal => println!("You guessed it right."),
     }
     }
-}
--- second-episode/guessing_game/src/main.rs
+++ second-episode/guessing_game/src/main.rs
@@ -1,15 +1,19 @@
 use std::io;
+// use rand::Rng;
 
 fn main() {
-    println!("Guessing Game!");
-
-    println!("Please input your guess: ");
+    println!("Langar Burja");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line.");
-    
-    println!("You guessed the number: {guess}");
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please input your guess: ");
+
+    loop {
+        // let secret_number = rand::thread_rng().gen_range(1..=6);
+        println!("{secret_number}");
+    };
 }
